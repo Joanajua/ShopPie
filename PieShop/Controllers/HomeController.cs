3ffb1800dc4f88c205668dccd3573ba34094808c
Joana Boiso-Ayan <J.Ayan@ucas.ac.uk>
2020-11-10 09:07:51 +0000

    Added Home controller with Index View, and created partial views for _PieCard and _Carousel

---
+++ PieShop/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PieShop.Data;
+using PieShop.ViewModels;
+
+namespace PieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PiesOfTheWeek = _pieRepository.PiesOfTheWeek
+            };
+            return View(homeViewModel);
+        }
+    }
+}
