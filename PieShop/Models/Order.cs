6fd7b46eccf251a0b8a2df7b1571df3359ece571
Joana Boiso-Ayan <J.Ayan@ucas.ac.uk>
2020-11-16 15:29:53 +0000

    Created Order and OrderDetail classes

---
+++ PieShop/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShop.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
+        public string Country { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public decimal OrderTotal { get; set; }
+        public DateTime OrderPlacedTime { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+
+    }
+}
---
+++ PieShop/Models/OrderDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShop.Models
+{
+    public class OrderDetail
+    {
+        public int OrderDetailId { get; set; }
+        public int OrderId { get; set; }
+        public string PieId { get; set; }
+        public int Amount { get; set; }
+        public decimal Price { get; set; }
+        public Pie Pie { get; set; }
+        public Order Order { get; set; }
+
+    }
+}
